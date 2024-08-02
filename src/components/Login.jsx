54d67cbf50c511c291958535cac0ade48bd0a2ba
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-08-02 17:49:13 +0500

    react-cotext-api concept

---
+++ src/components/Login.jsx
@@ -0,0 +1,26 @@
+import React, { useContext, useState } from 'react'
+import UserContext from '../context/UserContext'
+
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setpassword] = useState('')
+
+    const { setUser } = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUser({ username, password })
+
+    }
+    return (
+        <>
+            <h2>Login</h2>
+            <input type="text" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} />
+            {"   "}
+            <input type="password" placeholder='password' value={password} onChange={(e) => setpassword(e.target.value)} />
+            <button onClick={handleSubmit}>Submit</button>
+        </>
+    )
+}
+
+export default Login
